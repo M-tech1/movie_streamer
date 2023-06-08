3e22d70c7fec56f8c23df4b4f3d07520443cfa89
Martins Ake <akemartins13@gmail.com>
2023-06-08 13:16:08 +0100

    added somtin😎

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'stream/css/navigation';
+// import 'aos';
+// import 'aos/dist/aos.css';
+// import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +17,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
